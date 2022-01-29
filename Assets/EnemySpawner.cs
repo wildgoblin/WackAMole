f6c5ece42f9acc0b3296a2aaa81c5ad25bc6f289
Wild Goblin <evan@wildgoblin.com>
2022-01-29 09:33:17 -0800

    Added Spawn Availability List

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -8,16 +8,23 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] int minSpawnDelay;
     [SerializeField] int maxSpawnDelay;
 
+    List<int> spawnersAvailable = new List<int>();
+
     List<GameObject> spawners = new List<GameObject>();
 
     bool spawn = true;
 
     private void Start()
     {
+        //Initilization
         GetSpawnLocations();
+        GetSpawnersAvailable();
 
+        //Start Game
         StartCoroutine(Spawn());
     }
+
+
 
     private void GetSpawnLocations()
     {
@@ -27,13 +34,39 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
+    private void GetSpawnersAvailable()
+    {
+        int spawnerNumber = 0;
+        foreach (GameObject spawner in spawners)
+        {            
+            if (spawner.transform.childCount == 0)
+            {
+                Debug.Log("Added: " + spawnerNumber);
+                spawnersAvailable.Add(spawnerNumber);
+            }
+            else
+            {
+                Debug.Log("Something here: " + spawnerNumber);
+            }
+            spawnerNumber++;
+        }
+    }
+
     private IEnumerator Spawn()
-    {        
-        while(spawn)
+    {
+
+        while (spawn)
         {
+
+
             yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
-            int spawner = Random.Range(0, spawners.Count);
-            Instantiate(enemyPrefab, spawners[spawner].transform.position, transform.rotation, spawners[spawner].transform);
+
+            int spawner = Random.Range(0, spawners.Count); // Get random spawner
+            Instantiate(
+                enemyPrefab,
+                spawners[spawner].transform.position, // at random spawner location
+                transform.rotation, // Don't change rotation
+                spawners[spawner].transform); // Child new object under spawner
         }
     }
 }
