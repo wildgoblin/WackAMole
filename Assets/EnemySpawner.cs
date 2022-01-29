04c9798654256239f051fdc8db3ab1a307f3bb23
Wild Goblin <evan@wildgoblin.com>
2022-01-29 08:18:22 -0800

    Game Space & Bunny spawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] int minSpawnDelay;
+    [SerializeField] int maxSpawnDelay;
+
+    List<GameObject> spawners = new List<GameObject>();
+
+    bool spawn = true;
+
+    private void Start()
+    {
+        GetSpawnLocations();
+
+        StartCoroutine(Spawn());
+    }
+
+    private void GetSpawnLocations()
+    {
+        foreach (Transform spawner in this.transform)
+        {
+            spawners.Add(spawner.gameObject);
+        }
+    }
+
+    private IEnumerator Spawn()
+    {        
+        while(spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            int spawner = Random.Range(0, spawners.Count);
+            GameObject newBunny = Instantiate(enemyPrefab, spawners[spawner].transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+}
